db02edca8b6aeb6c33550e3077929a1c4b6e4248
Aaloke-Pandher <98190131+Aaloke-Pandher@users.noreply.github.com>
2024-01-17 13:13:14 -0700

    Update main.py

--- main.py
+++ main.py
@@ -22,22 +22,36 @@ SCREEN_HEIGHT = TILE_SIZE * COLUMNS
 # Player class 
 class Player:
     def __init__(self, x, y, w, h ,c):
-        self.x = x 
-        self.y = y 
+        self.parts = [[x, y], [x - TILE_SIZE, y], [x - 2 * TILE_SIZE, y]] 
         self.w = w 
         self.h = h  
         self.c = c  
-        self.yVel = 0
-        self.xVel = 0
-        self.speed = 2  
-        self.direction = 1 
+        self.direction = "right"
 
-    def drawPlayer(self, screen): 
-        pygame.draw.rect(screen, self.c, [self.x, self.y, self.w, self.h]) 
+    def drawPlayer(self, screen):
+        for part in self.parts:
+            pygame.draw.rect(screen, self.c, [part[0], part[1], self.w, self.h]) 
     
     def update(self):
-        self.x += self.xVel * TILE_SIZE
-        self.y += self.yVel * TILE_SIZE
+        cur_head = self.parts[0]
+
+        if self.direction == "right":
+            new_head = [cur_head[0] + TILE_SIZE, cur_head[1]]
+            self.parts.insert(0, new_head)
+            self.parts.pop()
+        elif self.direction == "up":
+            new_head = [cur_head[0], cur_head[1] - TILE_SIZE]
+            self.parts.insert(0, new_head)
+            self.parts.pop() 
+        elif self.direction == "down":
+            new_head = [cur_head[0], cur_head[1] + TILE_SIZE]
+            self.parts.insert(0, new_head)
+            self.parts.pop() 
+        elif self.direction == "left":
+            new_head = [cur_head[0] - TILE_SIZE, cur_head[1]]
+            self.parts.insert(0, new_head)
+            self.parts.pop()
+        
 
 
 class Food: 
@@ -84,51 +98,35 @@ class PowerUpCosmetic:
     def drawPowerUpCosmetic(self, screen):   
         pygame.draw.ellipse(screen, self.c, [self.x, self.y, self.w, self.h]) 
 
-# Find mouse 
-def mouse_position():
-    pos = pygame.mouse.get_pos() 
-    mouse_x = pos[0]
-    mouse_y = pos[1]
-    return mouse_x, mouse_y   
-
-# Player follow mouse 
-def follow_object(ob2, speed):  
-    run = (mouse_position()[0] - ob2.x) 
-    rise = (mouse_position()[1] - ob2.y) 
-    d = math.sqrt(rise**2 + run**2)
-    dy = (speed * rise) / d 
-    dx = (speed * run) / d 
-    ob2.x += dx
-    ob2.y += dy
-    return dx, dy 
-
 # Lists 
 food = [] 
 badFood = [] 
 powerUP = [] 
 cosmeticPowerUp = [] 
 playerList = [] 
-snake_parts = []
+
 
 
 # Collision Function 
 def rectCollision(rect1, rect2): 
-   return rect1.x < rect2.x + rect2.w and rect1.y < rect2.y + rect2.h and rect1.x + rect1.w > rect2.x and rect1.y + rect1.h > rect2.y 
+   for part in rect1.parts:
+    return part[0] < rect2.x + rect2.w and part[1] < rect2.y + rect2.h and part[0] + rect1.w > rect2.x and part[1] + rect1.h > rect2.y 
 
 # Wall Collision 
-def wallCollision(rect, score): 
-    if rect.x > 800 - rect.w:  
-        print("Game Over, you scored: " + str(score) + " points!") 
-        pygame.quit()
-    elif rect.x < 0: 
-        print("Game Over, you scored: " + str(score) + " points!") 
-        pygame.quit()
-    elif rect.y > 600 - rect.h:
-        print("Game Over, you scored: " + str(score) + " points!") 
-        pygame.quit()  
-    elif rect.y < 0: 
-        print("Game Over, you scored: " + str(score) + " points!") 
-        pygame.quit() 
+def wallCollision(rect, score):  
+    for part in rect.parts:
+        if part[0] > 800 - rect.w:  
+            print("Game Over, you scored: " + str(score) + " points!") 
+            pygame.quit()
+        elif part[0] < 0: 
+            print("Game Over, you scored: " + str(score) + " points!") 
+            pygame.quit()
+        elif part[1] > 600 - rect.h:
+            print("Game Over, you scored: " + str(score) + " points!") 
+            pygame.quit()  
+        elif part[1] < 0: 
+            print("Game Over, you scored: " + str(score) + " points!") 
+            pygame.quit() 
 
 player = Player(TILE_SIZE  * 20, TILE_SIZE * 15, TILE_SIZE, TILE_SIZE, GREEN) 
 # Main function 
@@ -157,18 +155,14 @@ def main():
             if event.type == pygame.QUIT: 
                 done = True  
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP and player.yVel != 1:
-                    player.xVel = 0 
-                    player.yVel = -1 
-                if event.key == pygame.K_RIGHT and player.xVel != -1:
-                    player.xVel = 1 
-                    player.yVel = 0
-                if event.key == pygame.K_DOWN and player.yVel != -1:
-                    player.xVel = 0
-                    player.yVel = 1 
-                if event.key == pygame.K_LEFT and player.xVel != 1:
-                    player.xVel = -1
-                    player.yVel = 0
+                if event.key == pygame.K_UP and player.direction != "down":
+                    player.direction = "up"
+                if event.key == pygame.K_RIGHT and player.direction != "left":
+                    player.direction = "right"
+                if event.key == pygame.K_DOWN and player.direction != "up":
+                    player.direction = "down"
+                if event.key == pygame.K_LEFT and player.direction != "right":
+                    player.direction = "left"
 
         # Draw 
         screen.fill(BLACK)  
@@ -198,8 +192,8 @@ def main():
         for i in range(len(food)): 
             food[i].drawFood(screen) 
         for i in range(len(food)): 
-            if rectCollision(food[i], player): 
-                player.w = food[i].w + player.w 
+            if rectCollision(player, food[i]): 
+                player.parts.append((food[i].x, food[i].y)) 
                 pygame.mixer.Sound.play(chompSound)
                 food.pop(i) 
                 Score += 1
@@ -209,8 +203,8 @@ def main():
         for i in range(len(badFood)):
             badFood[i].drawBadFood(screen)
         for i in range(len(badFood)):
-            if rectCollision(badFood[i], player): 
-                player.w = player.w - badFood[i].w 
+            if rectCollision(player, badFood[i]): 
+                player.parts.pop()
                 pygame.mixer.Sound.play(ouchSound)
                 badFood.pop(i) 
                 Score -= 1 
@@ -220,27 +214,29 @@ def main():
         for i in range(len(powerUP)):
             powerUP[i].drawPowerUp(screen) 
         for i in range(len(powerUP)):
-            if rectCollision(powerUP[i], player): 
+            if rectCollision(player, powerUP[i]): 
                 powerUpCount += 1
                 pygame.mixer.Sound.play(levelUpSound)
                 powerUP.pop(i) 
-                if frameCount % (15 - powerUpCount) == 0:
-                    player.update
+                if frameCount % (15 - powerUpCount * 2) == 0:
+                    player.update()
                 break 
 
         # Draw cosmetic power up and power up collsion
         for i in range(len(cosmeticPowerUp)):
             cosmeticPowerUp[i].drawPowerUpCosmetic(screen) 
         for i in range(len(cosmeticPowerUp)):
-            if rectCollision(cosmeticPowerUp[i], player): 
+            if rectCollision(player, cosmeticPowerUp[i]): 
                 player.c = [random.randrange(0, 255), random.randrange(0, 255), random.randrange(0, 255)] 
                 pygame.mixer.Sound.play(levelUpSound)
                 cosmeticPowerUp.pop(i)
-                break
+                break 
 
+        # Quit if length of snake reaches 0
         if player.w == 0:
             pygame.quit() 
-            print("Game Over, you scored: " + str(Score) + " points!") 
+            print("Game Over, you scored: " + str(Score) + " points!")  
+
         pygame.display.flip() 
  
         # --- Limit frames
